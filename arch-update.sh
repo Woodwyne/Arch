8c0c29517f7ecd06939e1c0d9118a8dc42e1d2c1
Woodwyne <winfearless@tutanota.com>
2021-12-16 21:34:12 +0200

    Update script

--- arch-update.sh
+++ arch-update.sh
@@ -2,10 +2,12 @@
 
 if [ `id -u` -ne 0 ]
 then
-    echo "Need root permission"
+    echo "[-] Need root permission"
     exit 1
 fi
 
+echo "[+] Start update"
 sudo pacman -Suyy --noconfirm --needed # Update package and repository
 sudo pacman -Qqdt --noconfirm  # Removing all orphaned packages 
 sudo pacman -Scc --noconfirm   # Clearing the package cache 
+echo "[+] End script"
