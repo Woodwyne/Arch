3e4836d3f1cf2fed62ffc34720b42246e83a7687
Woodwyne <winfearless@tutanota.com>
2021-12-18 15:59:05 +0200

    Add if

--- arch-xfce4.sh
+++ arch-xfce4.sh
@@ -9,13 +9,14 @@ xfce4=(
     lightdm-gtk-greeter-settings
 )
 
-if [ `id -u` -eq 0 ]
+if [ `id -u` -ne 0 ]
 then
-    for soft in "${xfce4[@]}"
-    do
-        sudo pacman -S $soft
-    done
-    systemctl enable lightdm
-else
-    echo "Need root permission"
+    echo "[-] Need root permission"
+    exit 1
 fi
+
+for soft in "${xfce4[@]}"
+do
+    sudo pacman -S $soft
+done
+sudo  systemctl enable lightdm
